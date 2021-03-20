a38b5f4d1e312ce7a5528e585c1c32b26bf30e04
Chris Wenz <christopherwenz@landmark.edu>
2021-03-19 21:16:14 -0400

    whole bunch of updates based on new applications

--- r/data2.r
+++ r/data2.r
@@ -1,26 +1,31 @@
 library(tibble)
 ##### EXPERIENCE #####
 experience <- tribble(~title, ~employer, ~startYear,~endYear, ~where, ~why,
+    # UCONN adjunct
+    "Adjunct Professor", "University of Connecticut", 2021, "Present", "Storrs, CT",
+      #First Bullet
+    "Design and deliver 'Teaching Reading and Writing in the Content Areas', a required course for all undergraduate teaching candidates in English, Science, Social Studies, Math, World Languages and Music.",
+                      
     # LCIRT    
     "Research Scientist", "Landmark College Institute for Research and Training (LCIRT)", 
     2018, 2020, "Putney, VT", 
         # First Bullet
-        "Design and deliver professional development for K-12 and post-secondary educators through workshops, online graduate courses, and Landmark's annual Summer Institute for Educators.",
+        "Designed and delivered professional development for K-12 and post-secondary educators through workshops, online graduate courses, and Landmark’s annual Summer Institute for Educators.",
     #
-    "Research Scientist", "Landmark College Institute for Research and Training (LCIRT)", 
-    2018, 2020, "Putney, VT", 
+    #"Research Scientist", "Landmark College Institute for Research and Training (LCIRT)", 
+    #2018, 2020, "Putney, VT", 
         # Second Bullet
-        "Recruit, train and mentor Landmark Students for internships in our neuro-cognitive lab.",
+        #"Recruit, train and mentor Landmark Students for internships in  neuro-cognitive lab.",
     #
     "Research Scientist", "Landmark College Institute for Research and Training (LCIRT)", 
     2018, 2020, "Putney, VT", 
         # Third Bullet
-        "Conduct original research that expands our understanding of neurodiversity and pedagogical practices that meet the specific needs of neurodivergent learners.", 
+        " Sought funding for and conducted original research on universal design for learning, neurodiversity and instruction for students with learning differences.", 
     #
     "Research Scientist", "Landmark College Institute for Research and Training (LCIRT)", 
     2018, 2020, "Putney, VT", 
     # Fourth Bullet
-    "Serve the College as member of the Center for Neurodiversity Steering Committee and Pedagogy Committee.", 
+    "Served the college as a member of the Center for Neurodiversity Steering Committee and Pedagogy Committee.", 
     
 
     # Graduate Assistant   
@@ -42,7 +47,7 @@ experience <- tribble(~title, ~employer, ~startYear,~endYear, ~where, ~why,
     "Graduate Writing Tutor", "University Writing Center --- University of Connecticut",
     2013, 2018, "Storrs, CT", 
     # First Bullet
-    "Supported undergraduate writers through individual writing conferences",
+    "Supported undergraduate and graduate writers through individual writing conferences.",
     #
     "Graduate Writing Tutor", "University Writing Center --- University of Connecticut", 
     2013, 2018, "Storrs, CT", 
@@ -53,7 +58,7 @@ sciences.",
     "Graduate Writing Tutor", "University Writing Center --- University of Connecticut",
     2013, 2018, "Storrs, CT", 
     # Second Bullet
-    "Designed workshop in learning differences and disabilities for 30 undergraduate and graduate tutors.",
+    "Designed workshop in learning differences and disabilities for 40 undergraduate and graduate tutors.",
     #
     # CSD 
     "Strategy Instructor", "Center for Students with Disabilities---University of Connecticut", 
@@ -75,20 +80,20 @@ sciences.",
     "Residential Dean and Humanities Instructor", "Franklin Academy", 
     2007, 2012, "East Haddam, CT", 
     # Second Bullet
-    "Supervised a 15 member residential duty team responsbile for the safety and wellbeing of 80+ autistic students.",
+    "Managed a 15 member residential duty team responsbile for the safety and wellbeing of 80+ autistic students.",
     #
     "Residential Dean and Humanities Instructor", "Franklin Academy", 
     2007, 2012, "East Haddam, CT", 
     # Third Bullet
-    "Designed and taught courses in History, English, songwriting, and social justice.",
+    "Designed and taught courses in English, History, songwriting, and social justice.",
 )
     
 #### EDUCATION SECTION #####
 
 education <- tribble(~school, ~degree, ~year, ~where,
-    "University of Connecticut", "PhD, Curriculum & Instruction", "May 2021", "Storrs, CT", 
+    "University of Connecticut", "PhD, Curriculum & Instruction", "Aug 2021", "Storrs, CT", 
     "University of Connecticut", "MA, Educational Psychology", "2014", "Storrs, CT",
-    "Hampshire College", "BA, History & Cognitive Science", "2007", "Amherst, MA"
+    "Hampshire College", "BA, History", "2007", "Amherst, MA"
     ) 
   
 graduate <- tribble(~school, ~degree, ~year, ~where,
@@ -108,6 +113,21 @@ educationDETAIL <- tribble(~school, ~city, ~degree, ~Year,
                       "University of Connecticut", "Storrs, CT", "MA --- Educational Psychology", "2014",
                       "Hampshire College", "Amherst, MA", "BA -- History and Cognitive Science", "2007",
                       )
+
+
+skillsDetail <- tribble(
+  ~area, ~skills,
+  "Data Analysis", "R, SPSS, SPSS Amos, MPlus, ATLAS.ti, NVivo",
+  "Learning Management Systems", "Canvas, Blackboard, Moodle",
+  "Markup Languages", "HTML, LaTeX, Markdown, RMarkdown, CSS", 
+  "Text Editors", "RStudio, Atom",
+  "Adobe Creative Cloud","Illustrator, Photoshop, InDesign, Lightroom",
+  "Microsoft Office", "Excel,PowerPoint, Word, Outlook, OneNote",
+
+)
+
+
+
 
 #### Old code for brief entries education 
 # Education
